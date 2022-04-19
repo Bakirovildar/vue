216df3e1f6f13981337fcdccd1682a50d2bc7b62
Ildar <bakirovildar2011@mail.ru>
2022-04-19 10:57:28 +0500

    Добавил vuex, mutations для счетчика

---
+++ vuex/src/TheNavbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="navbar">
+       <h1> {{ $store.state.counter }} </h1>
+       <button class="btn" @click="add">Добавить</button>
+  </header>
+</template>
+
+<script>
+  export default {
+    methods: {
+      add() {
+        this.$store.commit('add',{
+          value: 10,
+        })
+      }
+    }
+  }
+</script>
